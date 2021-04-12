4391b6d8734d14bf1b86deb8ea5104df8e5cb628
hyupkim <makeyeezy@gmail.com>
2021-04-11 22:12:17 -0400

    add .gitignore

--- todolist/settings.py
+++ todolist/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import django_heroku
 import dj_database_url
+from secret_key import secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$$l(y*e-k#tjnhdvng=xs67*e1=q+sw&y0mxj*^8uznla3iyeh'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
